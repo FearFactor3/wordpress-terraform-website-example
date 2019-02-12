658e84687369921eea209938544ba980c3c3e545
Wayne Major <wayne.major03@gmail.com>
2019-02-12 12:34:10 -0500

    Working version of Wordpress/Terraform/Ansible project I have worked on thus far.

--- variables.tf
+++ variables.tf
@@ -21,3 +21,10 @@ variable "dev_instance_type" {}
 variable "dev_ami" {}
 variable "public_key_path" {}
 variable "key_name" {}
+variable "asg_max" {}
+variable "asg_min" {}
+variable "asg_grace" {}
+variable "asg_hct" {}
+variable "asg_cap" {}
+variable "lc_instance_type" {}
+variable "delegation_set" {}
